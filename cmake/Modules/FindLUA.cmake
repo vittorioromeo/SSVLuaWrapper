6befb5bbb3a99910d8db0c41682c59a99976f655
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-05-20 17:38:00 +0200

    submodule update

--- cmake/Modules/FindLUA.cmake
+++ cmake/Modules/FindLUA.cmake
@@ -17,7 +17,7 @@ message("\nFound Lua include at: ${LUA_INCLUDE_DIR}.\n")
 FIND_LIBRARY(LUA_LIBRARY
   NAMES lua5.1.lib liblua5.1 lua5.1 lua liblua
   PATHS "${PROJECT_SOURCE_DIR}/../lua/"
-  PATH_SUFFIXES lib/ lib64/ lua/ lua/lib/ lua/lib64/ ./
+  PATH_SUFFIXES lib/ lib/x86_64-linux-gnu/ lib64/ lua/ lua/lib/ lua/lib64/ ./
   "${PROJECT_SOURCE_DIR}/extlibs/lua/"
   ${LUA_ROOT}
   $ENV{LUA_ROOT}
@@ -27,6 +27,7 @@ FIND_LIBRARY(LUA_LIBRARY
   /opt/local/
   /opt/csw/
   /opt/
+  /usr/lib/x86_64-linux-gnu
 )
 
 message("\nFound Lua library at: ${LUA_LIBRARY}.\n")
