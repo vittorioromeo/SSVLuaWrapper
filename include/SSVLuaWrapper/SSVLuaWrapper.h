9c438e489c6211f2d7ea70562c29bf44ca15ce94
Vittorio Romeo <vittorio.romeo@outlook.com>
2013-06-21 16:29:31 +0200

    license updated

--- include/SSVLuaWrapper/SSVLuaWrapper.h
+++ include/SSVLuaWrapper/SSVLuaWrapper.h
@@ -1,1 +1,10 @@
+// Copyright (c) 2013 Vittorio Romeo
+// License: Academic Free License ("AFL") v. 3.0
+// AFL License page: http://opensource.org/licenses/AFL-3.0
+
+#ifndef SSVLUAWRAPPER
+#define SSVLUAWRAPPER
+
 #include "SSVLuaWrapper/LuaContext/LuaContext.h"
+
+#endif
